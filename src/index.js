60717394d033f3e065360b3d98049a56785fafd9
Zac <61070171+zcohen-nerd@users.noreply.github.com>
2026-03-27 06:19:02 -0400

    Create index.js for theme entry point

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// Entry point for the literacy-site-theme
+
+// You can export your components here
+
+export { default as EcosystemLinks } from './components/EcosystemLinks';
+export { default as LiteracyNavbar } from './components/LiteracyNavbar';
+export { default as LiteracyFooter } from './components/LiteracyFooter';
